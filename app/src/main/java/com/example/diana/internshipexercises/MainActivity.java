c28e4c1c9398d067bec9d71b38c41a1acf7af315
RalucaOniciuc <oniciucralucaf@gmail.com>
2020-03-09 12:45:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/diana/internshipexercises/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.diana.internshipexercises;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG =MainActivity.class.getSimpleName();
+
+    private int incrementValue=0;
+
+    private TextView incrementTV;
+    private Button incrementBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "OnCreate: Happy to be born!");
+        initView();
+    }
+
+    private void initView(){
+        incrementTV=findViewById(R.id.counter_value_tv);
+        incrementBtn=findViewById(R.id.increment_bt);
+
+        incrementTV.setText(incrementValue+"");
+        incrementBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(incrementTV.getText().toString().isEmpty()){
+                    incrementValue++;
+                }
+                else{
+                    incrementValue=Integer.parseInt(incrementTV.getText().toString());
+                    incrementValue++;
+                }
+
+                incrementTV.setText(incrementValue+ "");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "OnStart: I exist, but you can't see me");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"OnResume: Preparing");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"OnPause: You can see me");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"OnStop: Packing up");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"OnDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("label_key", incrementTV.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        incrementTV.setText(savedInstanceState.getString("label_key"));
+    }
+}
